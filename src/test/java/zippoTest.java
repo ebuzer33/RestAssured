55c660ed21f3a36be1aa57be1dfd8a57e3b0466b
ebuzer33 <ebuzer.tutuk@gmail.com>
2021-08-24 21:29:16 +0300

    Initial commit

---
+++ src/test/java/zippoTest.java
@@ -0,0 +1,21 @@
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class zippoTest {
+
+    @Test
+    public void test()
+    {
+        given()
+                // hazirlik islemleri
+                .when()
+                // link ve aksiyon islemleri
+
+                .then()
+                // test ve extract islemleri
+        ;
+    }
+}
