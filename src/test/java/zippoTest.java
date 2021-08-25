d5a34fa6dd6e4e6819c883f97a2f7f65b27577b6
ebuzer33 <ebuzer.tutuk@gmail.com>
2021-08-25 20:14:52 +0300

    bodyJsonPathTestHasItem added

--- src/test/java/zippoTest.java
+++ src/test/java/zippoTest.java
@@ -90,4 +90,20 @@ public class zippoTest {
                 .statusCode(200)
         ;
     }
+
+    @Test
+    public void bodyJsonPathTestHasItem()
+    {
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .body("places.'state'", hasItem("California1"))
+                .statusCode(200)
+        ;
+    }
+
 }
