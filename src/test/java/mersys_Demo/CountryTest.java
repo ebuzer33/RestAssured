884c2711ad256a0693709dc7539ab1a2494421b4
ebuzer33 <ebuzer.tutuk@gmail.com>
2021-09-16 21:17:08 +0300

    updated

--- src/test/java/mersys_Demo/CountryTest.java
+++ src/test/java/mersys_Demo/CountryTest.java
@@ -131,7 +131,7 @@ public class CountryTest {
                 .delete("/school-service/api/countries/{countryId}")
 
                 .then()
-                .statusCode(200)
+                .statusCode(201)
                 .log().body()
         ;
     }
